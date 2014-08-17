b54d09516619998745e56a997a8a3e2fc11e1b3a
Andrea Usuelli <andrea.usuelli@facile.it>
2014-08-17 14:54:59 +0200

    Added unrar functionality

--- src/Usu/TvShowsRenamer/Renamer.php
+++ src/Usu/TvShowsRenamer/Renamer.php
@@ -3,16 +3,42 @@
 namespace Usu\TvShowsRenamer;
 
 use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Process\Process;
 
 class Renamer {
 
-    static public function rename($absoluteFilePath)
+    static public function rename($absolutePath)
     {
         $seriesName = null;
         $seriesSeason = null;
         $seriesEpisode = null;
 
-        $dirName = dirname($absoluteFilePath);
+        if (is_file($absolutePath)) {
+
+        } else {
+            $files = scandir($absolutePath);
+            foreach ($files as $file) {
+                if (preg_match('/\.rar/i', $file)) {
+                    $process = new Process('unrar e -o- ' . $absolutePath . '/' . $file . ' ' . $absolutePath);
+                    $process->run(function ($type, $buffer) {
+                        if (Process::ERR === $type) {
+                            echo 'ERR > '.$buffer;
+                        } else {
+                            echo $buffer;
+                        }
+                    });
+                    $files2 = scandir($absolutePath);
+                    foreach ($files2 as $f) {
+                        if (preg_match('/\.(avi|mp4|mkv)/i', $f)) {
+                            $absolutePath = $absolutePath . '/' . $f;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        $dirName = dirname($absolutePath);
 
         if (preg_match('/([\w\.\s]+)(?:S(\d{1,3})E(\d{1,3})|(\d{1,3})x(\d{1,3})|(\d{3,5})).+/', $dirName, $matches)) {
             //var_dump($matches);
@@ -24,31 +50,31 @@ class Renamer {
             throw new \UnexpectedValueException('Unrecognized format! ' . $dirName);
         }
 
-        $newFileName = $series->name . ' S' . $matches[2] . 'E' . $matches[3] . ' - ' . ucfirst(strtolower($episodeTitle)) . '.' . pathinfo($absoluteFilePath, PATHINFO_EXTENSION);
-
-        $baseDir = pathinfo($absoluteFilePath, PATHINFO_DIRNAME);
-        $fs = new \Symfony\Component\Filesystem\Filesystem();
+        $newFileName = $series->name . ' S' . $matches[2] . 'E' . $matches[3] . ' - ' . ucfirst(strtolower($episodeTitle)) . '.' . pathinfo($absolutePath, PATHINFO_EXTENSION);
 
         $yaml = new Parser();
 
         $config = $yaml->parse(file_get_contents(__DIR__ . '/../../../config/parameters.yml'));
-        $newFolder = $config['fs']['series_folder'] . '/' . $series->name . '/Season ' . intval($matches[2]);
+        $destinationFolder = $config['fs']['series_folder'] . '/' . $series->name . '/Season ' . intval($matches[2]);
+
+        if (file_exists($destinationFolder . '/' . $newFileName)) {
+            throw new \Exception($destinationFolder . '/' . $newFileName . ' already exists');
+        }
+
+        $baseDir = pathinfo($absolutePath, PATHINFO_DIRNAME);
+        $fs = new \Symfony\Component\Filesystem\Filesystem();
 
         if ($fs->exists($config['fs']['series_folder'])) {
-            /*if (!$fs->exists($config['fs']['series_folder'] . '/' . $series->name)) {
-                mkdir($config['fs']['series_folder'] . '/' . $series->name, 0777, true);
-            }*/
-            if (!file_exists($newFolder)) {
-                mkdir($newFolder, 0777, true);
+            if (!file_exists($destinationFolder)) {
+                mkdir($destinationFolder, 0777, true);
             }
-            $fs->rename($absoluteFilePath, $config['fs']['series_folder'] . '/' . $series->name . '/Season ' . intval($matches[2]) . '/' . $newFileName);
+            $fs->rename($absolutePath, $config['fs']['series_folder'] . '/' . $series->name . '/Season ' . intval($matches[2]) . '/' . $newFileName);
         }
 
         $files = scandir($baseDir);
         foreach ($files as $file) {
             if (preg_match('/\.srt/i', $file)) {
-                echo 'rinominato!';
-                $fs->rename($baseDir . '/' . $file, $newFolder . '/' . $series->name . ' S' . $matches[2] . 'E' . $matches[3] . ' - ' . ucfirst(strtolower($episodeTitle)) . '.srt');
+                $fs->rename($baseDir . '/' . $file, $destinationFolder . '/' . $series->name . ' S' . $matches[2] . 'E' . $matches[3] . ' - ' . ucfirst(strtolower($episodeTitle)) . '.srt');
             }
         }
 
